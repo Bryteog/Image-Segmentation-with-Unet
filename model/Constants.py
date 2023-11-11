3740e2f5f67ea1e2b5030a1ae16438a2e4fc0798
Bright Gyamfi <91860731+Bryteog@users.noreply.github.com>
2023-11-11 04:33:52 +0000

    Add files via upload

---
+++ model/Constants.py
@@ -0,0 +1,12 @@
+import os
+
+NUM_FILTERS = 32
+IMAGE_HEIGHT = 96
+IMAGE_WIDTH = 128
+NUM_CHANNELS = 3
+INPUT_SHAPE = (96, 128, 3)
+BATCH_SIZE = 32
+BUFFER_SIZE = 1000
+EPOCHS = 50
+
+#image_dir = os.path.join(path, )
